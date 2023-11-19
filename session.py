025b7c7a7a4b9eea78ff8ca4e8d9e5075bd0d8e1
Utkucan Doğan <41344400+utkucandogan@users.noreply.github.com>
2023-11-19 22:12:43 +0300

    additional session name strategy

--- session.py
+++ session.py
@@ -6,23 +6,26 @@ class Session:
             self.pwd: str      = os.path.join(pwd, config.get("pwd", ""))
             self.sessions: str = os.path.join(self.pwd, config["sessions"])
 
-            self.session_list: list[list[str]] = config["session_list"]
-
             self.session_name_format: str = config.get("session_name_format", "Session-{no}")
             self.table_name_format: str   = config.get("table_name_format", "Table-{no}")
             self.use_folder_name: bool    = config.get("use_folder_name", False)
+
+            if isinstance(config["session_list"], dict):
+                self.session_list: dict[str, list[str]] = config["session_list"]
+            else:
+                self.session_list: dict[str, list[str]] = { self.session_name_format.format(no=i+1): v for i, v in enumerate(config["session_list"]) }
 
         except KeyError as e:
             raise RuntimeError(f"Config file requires {e} in \"session\" entry.")
         
     # This function finds the correct session for the student. In order to function correctly,
     # student's zip file must contain their id in the filename
-    def find(self, filename: str) -> tuple[tuple[int, int], str] | tuple[None, None]:
-        for i, session in enumerate(self.session_list):
-            for j, id in enumerate(session):
+    def find(self, filename: str) -> tuple[str, int, str] | tuple[None, None, None]:
+        for session_name, session in self.session_list.items():
+            for i, id in enumerate(session):
                 if id in filename:
-                    return (i+1,j+1), id
-        return None, None
+                    return session_name, i+1, id
+        return None, None, None
     
     # This function groups submissions (or reports) into correct subfolders
     def group(self, path: str):
@@ -32,34 +35,32 @@ class Session:
                 continue
 
             if self.use_folder_name:
-                session, id = self.find(user)
+                session_name, table_no, id = self.find(user)
 
             for file in os.listdir(user):
                 if not self.use_folder_name:
-                    session, id = self.find(file)
+                    session_name, table_no, id = self.find(file)
 
                 print(f"Processing: {file}")
                 filepath = os.path.join(user, file)
                 new_path = ""
-                if session is None:
+                if id is None:
                     new_path = os.path.join(self.sessions, "ERROR", folder)
                     os.makedirs(new_path, exist_ok=True)
                 else:
-                    session_name = self.session_name_format.format(no=session[0])
-                    table_name = self.table_name_format.format(no=session[1], id=id)
+                    table_name = self.table_name_format.format(no=table_no, id=id)
                     new_path = os.path.join(self.sessions, session_name, table_name)
                 shutil.copy2(filepath, new_path)
 
     # This function creates folders and groups all the submissions
     def create(self, submissions: list[str]):
         os.makedirs(self.sessions, exist_ok=True)
-        for i, session in enumerate(self.session_list):
-            session_name = self.session_name_format.format(no=i+1)
+        for session_name, session in self.session_list.items():
             session_path = os.path.join(self.sessions, session_name)
             os.makedirs(session_path, exist_ok=True)
 
-            for j, id in enumerate(session):
-                table_name = self.table_name_format.format(no=j+1, id=id)
+            for i, id in enumerate(session):
+                table_name = self.table_name_format.format(no=i+1, id=id)
                 table_path = os.path.join(session_path, table_name)
                 os.makedirs(table_path, exist_ok=True)
 
